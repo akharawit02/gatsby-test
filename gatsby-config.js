12245b616392bade1cf70955627ec48500d8b0f3
Akharawit(NOT) <akharawit.aryakom@gmail.com>
2020-04-18 09:51:12 +0700

    Update public master branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/my_project",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -30,6 +31,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    (pathPrefix: "/gatsby-test"),
   ],
 }
